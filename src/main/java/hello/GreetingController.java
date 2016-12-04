31a72eed4eacb8800cfac4384f6da088c42c134b
Tibevilaqua <tiago.bevilaqua@hotmail.com>
2016-12-04 21:50:33 +1100

    first commit

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,24 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by root on 04/12/16.
+ */
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/greeting",method = RequestMethod.GET)
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
